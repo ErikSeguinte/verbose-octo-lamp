9ba245969719ba4e81e78035225d26b33b6d8860
Erik Seguinte <ErikSeguinte@gmail.com>
2024-02-21 17:48:32 -0800

    moves navbar to root layout

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -5,9 +5,8 @@ import Timezone from "./components/Timezone";
 export default function Home() {
   return (
     <>
-      <Navbar />
 
-      <h1 className="text-6xl text-center font-bold">YAGSA </h1>
+      <h1 className="text-6xl text-center font-bold pt-10 pb-8">YAGSA </h1>
       <h2 className="text-2xl text-center">Yet Another Group Scheduling App</h2>
 
       <Timezone />
