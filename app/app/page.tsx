c539b48ac65df323f8cb3f6467e9be5ed67d2a35
Erik Seguinte <erikseguinte@gmail.com>
2024-02-23 09:02:56 +0000

    more mantine changes

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Timezone from "@/components/TimezoneSelect";
+import { Title } from "@mantine/core"
 
 export default function Home() {
   return (
     <div className="flex-col gap-10"  >
 
-      <h1 className="text-6xl text-center font-bold pt-10 pb-8">YAGSA </h1>
-      <h2 className="text-2xl text-center">Yet Another Group Scheduling App</h2>
+      <Title className="text-6xl text-center font-bold pt-10">YAGSA </Title>
+      <h2 className="text-2xl text-center text-gray-500 pb-10">Yet Another Group Scheduling App</h2>
 
       <Timezone />
     </div>
