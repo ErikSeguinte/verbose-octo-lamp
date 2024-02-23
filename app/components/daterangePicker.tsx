32a3caf69ab9d0219a7d63c72025e752944f55ed
Erik Seguinte <erikseguinte@gmail.com>
2024-02-23 08:36:09 -0800

    adds little calendar icon

--- app/components/daterangePicker.tsx
+++ app/components/daterangePicker.tsx
@@ -2,10 +2,14 @@
 import React from 'react'
 import {useState} from "react"
 import { DatePickerInput } from '@mantine/dates';
+import { IconCalendar } from '@tabler/icons-react';
+import { rem } from '@mantine/core';
+
 
 
 const DaterangePicker = () => {
     const [value, setValue] = useState<[Date | null, Date | null]>([null, null]);
+    const icon = <IconCalendar style={{ width: rem(18), height: rem(18) }} stroke={1.5} />;
     return (
       <DatePickerInput
         type="range"
@@ -13,6 +17,8 @@ const DaterangePicker = () => {
         placeholder="Pick dates range"
         value={value}
         onChange={setValue}
+        leftSection={icon}
+        leftSectionPointerEvents="none"
       />
   )
 }
