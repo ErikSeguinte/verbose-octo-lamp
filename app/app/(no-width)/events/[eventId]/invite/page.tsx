f62891c33579ca811d7301cb7641883f9c0bd2a3
Erik Seguinte <ErikSeguinte@gmail.com>
2024-02-25 12:51:01 -0800

    fix merge

--- app/app/(no-width)/events/[eventId]/invite/page.tsx
+++ app/app/(no-width)/events/[eventId]/invite/page.tsx
@@ -1,32 +1,37 @@
-import React from 'react'
+"use client"
+import {Code, Space, TypographyStylesProvider } from "@mantine/core";
+import React from "react";
+import TimezoneSelect from "@/components/TimezoneSelect";
+import { useState } from "react";
 
-const Page = () => {
+const Page = ({ params }: { params: { eventId: string } }) => {
+    const [selectedTimezone, setSelectedTimezone] = useState<string | null>("");
   return (
     <div>
-
-
-<p>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nec tristique sem, ac consectetur lectus. Aenean libero risus, bibendum lacinia urna non, tristique vehicula neque. Aenean id lacus quis tellus ultricies eleifend. Pellentesque vel maximus sem, quis convallis tortor. Mauris cursus lorem id tortor ultricies, ac tristique enim lacinia. In hac habitasse platea dictumst. Ut tincidunt aliquet arcu, faucibus elementum orci. Fusce porttitor malesuada neque, vel fermentum purus ullamcorper in. Pellentesque ac quam mi. Maecenas tortor mi, suscipit imperdiet elit molestie, mattis consequat augue.
-</p>
-
-<p>
-Cras laoreet elementum ligula, sed egestas lectus ullamcorper non. In vulputate augue sed erat suscipit cursus. Vivamus pretium fermentum bibendum. Pellentesque porta tellus in arcu consectetur suscipit. Maecenas non bibendum nibh. Fusce ex velit, congue id molestie quis, pulvinar vitae lorem. Suspendisse potenti. Duis dapibus purus nec auctor ornare. Fusce eu lectus et justo volutpat semper. Nunc eget commodo orci. Maecenas rhoncus lorem vitae leo egestas, at tempus erat dapibus. Donec ac sollicitudin dui, nec aliquam nibh.
-</p>
-
-<p>
-Vivamus at leo finibus, auctor mauris non, interdum leo. Vivamus ac tincidunt erat. Vestibulum sed dui nec erat faucibus pellentesque et vitae est. Vivamus fermentum justo vitae dolor hendrerit, a sagittis nulla rhoncus. Suspendisse finibus mattis dui, bibendum fringilla felis fermentum in. Nulla sollicitudin orci at nisl elementum, ut consectetur urna facilisis. Sed dignissim velit neque, in venenatis turpis commodo at. Vestibulum pulvinar mollis felis nec congue. Duis eget felis non odio auctor semper. Phasellus vel lacinia ipsum.
-</p>
-
-<p>
-Ut pellentesque bibendum felis eu elementum. Nam magna arcu, euismod nec ipsum nec, lobortis pellentesque augue. Phasellus convallis, augue et tristique porta, augue dolor sagittis nunc, at euismod nunc enim id risus. Mauris vitae augue enim. Nullam in sagittis urna. Sed vulputate dignissim ipsum a pulvinar. Integer venenatis vehicula arcu iaculis finibus. Fusce pellentesque iaculis nibh, sit amet imperdiet ligula auctor at. Phasellus id dapibus ante. Nunc pulvinar lobortis molestie. Donec porttitor sed risus sed dictum. Maecenas ultricies nulla fringilla neque feugiat, ac iaculis diam ultrices. Sed a luctus mauris. Sed nec maximus augue, sed tempus lectus. Aliquam erat volutpat. Nunc sit amet velit vitae orci dapibus ultricies.
-</p>
-
-<p>
-Ut sit amet nisl sagittis, finibus urna quis, blandit turpis. Nunc convallis odio sit amet auctor auctor. Aliquam at rhoncus orci, id laoreet ex. Curabitur urna nibh, accumsan sed sem non, euismod efficitur ante. Mauris id massa quis sem feugiat finibus at et libero. Mauris scelerisque vestibulum fermentum. Donec pretium a risus varius lacinia. Nunc ex tellus, sagittis sit amet purus vel, laoreet luctus augue. Ut eu vulputate libero, vel sollicitudin turpis. Phasellus scelerisque vestibulum diam id imperdiet. In hac habitasse platea dictumst. Aliquam sit amet arcu ultricies, eleifend nunc a, ullamcorper lacus. Nam eu cursus felis. Quisque ac elit porttitor, porttitor arcu et, rutrum sapien. Phasellus ut sapien enim. Donec ultrices lacus dolor, at interdum sem vestibulum quis. 
-</p>
-
+      {" "}
+      <TypographyStylesProvider>
+        <h2> Select your timezone. </h2>
+        <p>
+          Events often span multiple timezones. To ensure that availabilities
+          are properly synchronized, please ensure that the correct timezone is
+          chosen below. The select box will have attempted its best guess at
+          your current timezone based on your browser settings. The textbox
+          itself is searchable, as there are a metric shit-ton of timezones, so
+          it may be easier to start typing and filtering them. Timezones are in
+          the format <Code>America/Los Angeles</Code> or{" "}
+          <Code>Europe/London</Code>.
+        </p>
+      </TypographyStylesProvider>
+      <Space h="md" />
+      <div className="">
+        <TimezoneSelect
+          selectedTimezone={selectedTimezone}
+          setSelectedTimezone={setSelectedTimezone}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+
+export default Page;
