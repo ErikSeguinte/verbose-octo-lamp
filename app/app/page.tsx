736379c4484640141903b7b151ab581e65cc9e26
Erik Seguinte <erikseguinte@gmail.com>
2024-02-28 21:47:09 -0800

    Octolam Page!

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,12 +1,65 @@
-import { Title } from "@mantine/core";
+import { Button, Container, Group, Text, Title } from "@mantine/core";
+import { IconBrandGithub } from "@tabler/icons-react";
+import Image from "next/image";
 
-export default function Home() {
+import octolamp from "@/assets/octolamp.png";
+
+import classes from "./HeroTitle.module.css";
+
+export default function HeroTitle() {
   return (
-    <div className="flex-col gap-10">
-      <Title className="text-6xl text-center font-bold pt-10">YAGSA </Title>
-      <h2 className="text-2xl text-center text-gray-500 pb-10">
-        Yet Another Group Scheduling App
-      </h2>
+    <div className={classes.wrapper}>
+      <div className="flex w-auto justify-center">
+        <Group
+          className="flex w-auto justify-center"
+          justify="center"
+          mah="200px"
+          maw="1000px"
+        >
+          <Image
+            alt="An octopus lamp"
+            className="w-auto"
+            height="400"
+            src={octolamp}
+          />
+          <Container className={classes.inner} size={700}>
+            <h1 className={classes.title}>Verbose Octolamp</h1>
+            <h2 className="-5rem"> Yet another Group Scheduling App</h2>
+            <Text className={classes.description} color="dimmed">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ornare
+              arcu dui vivamus arcu felis bibendum ut tristique et. Netus et
+              malesuada fames ac turpis egestas maecenas pharetra.
+            </Text>
+
+            <Title mt="1rem" order={3}>
+              A Law of Names utility
+            </Title>
+
+            <Group className={classes.controls}>
+              <Button
+                className={classes.control}
+                gradient={{ from: "blue", to: "cyan" }}
+                size="xl"
+                variant="gradient"
+              >
+                Get started
+              </Button>
+
+              <Button
+                className={classes.control}
+                component="a"
+                href="https://github.com/mantinedev/mantine"
+                leftSection={<IconBrandGithub size={20} />}
+                size="xl"
+                variant="default"
+              >
+                GitHub
+              </Button>
+            </Group>
+          </Container>
+        </Group>
+      </div>
     </div>
   );
 }
